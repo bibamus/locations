a4afbdd026a19d27cf10ce485e9d7cf13e1abe5d
Patrick Cassel <patrick.cassel@googlemail.com>
2024-03-15 12:09:55 +0100

    in memory data

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,30 +1,56 @@
-use axum::{Json, Router, routing::get};
-use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
+use std::sync::{Arc, RwLock};
+
+use axum::{Json, Router};
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 struct CreatePlace {
     name: String,
 }
 
+
+#[derive(Debug, Serialize, Clone)]
+struct Place {
+    id: Uuid,
+    name: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/place",
-                                  get(|| async { "Hello, World!" })
-                                      .post(create_place),
-    )
-        .with_state("");
+    let db = Db::default();
+
+    let app = Router::new().route("/place", get(list_places).post(create_place))
+        .with_state(db);
 
 
-    let port = env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string());
-
-
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn create_place(Json(input): Json<CreatePlace>) -> String {
-    let name = input.name;
-    return format!("created {name}");
+async fn list_places(State(db): State<Db>) -> impl IntoResponse {
+    let places = db.read().unwrap()
+        .values()
+        .cloned()
+        .collect::<Vec<_>>();
+
+    return Json(places);
 }
+
+async fn create_place(State(db): State<Db>, Json(input): Json<CreatePlace>) -> impl IntoResponse {
+    let place = Place {
+        id: Uuid::new_v4(),
+        name: input.name,
+    };
+    db.write().unwrap().insert(place.id, place.clone());
+    return (StatusCode::CREATED, Json(place));
+}
+
+type Db = Arc<RwLock<HashMap<Uuid, Place>>>;
