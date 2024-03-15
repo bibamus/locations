1ba8e51c3afa2155f92ca7daa1290175939a3088
Patrick Cassel <patrick.cassel@googlemail.com>
2024-03-15 15:33:56 +0100

    postgres connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,7 +7,13 @@ use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
+use log::{debug, info};
+use native_tls::TlsConnector;
+use postgres_native_tls::MakeTlsConnector;
 use serde::{Deserialize, Serialize};
+use tokio_postgres::{Config, NoTls, Row};
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -22,12 +28,36 @@ struct Place {
     name: String,
 }
 
+type ConnectionPool = Pool<PostgresConnectionManager<MakeTlsConnector>>;
+// type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
+
+async fn init_db(pool: &ConnectionPool) {
+    info!("Initializing Databsae");
+    let conn = pool.get().await.unwrap();
+    conn.batch_execute("CREATE TABLE IF NOT EXISTS places (id UUID, name VARCHAR NOT NULL)").await.unwrap();
+    info!("Database initialised");
+}
+
 #[tokio::main]
 async fn main() {
-    let db = Db::default();
+    env_logger::init();
+
+    info!("Application starting.");
+
+    let config = get_postgres_config();
+    let connector = TlsConnector::builder()
+        .build()
+        .unwrap();
+    let connector = MakeTlsConnector::new(connector);
+    let manager = PostgresConnectionManager::new(config, connector);
+    // let manager = PostgresConnectionManager::new(config, NoTls);
+    let pool = Pool::builder().build(manager).await.unwrap();
+
+    init_db(&pool).await;
+
 
     let app = Router::new().route("/place", get(list_places).post(create_place))
-        .with_state(db);
+        .with_state(pool);
 
 
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
@@ -35,21 +65,48 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn list_places(State(db): State<Db>) -> impl IntoResponse {
-    let places = db.read().unwrap()
-        .values()
-        .cloned()
+fn get_postgres_config() -> Config {
+    let host = env::var("POSTGRES_HOST").unwrap();
+    let port: u16 = env::var("POSTGRES_PORT").unwrap().parse().unwrap();
+    let user = env::var("POSTGRES_USER").unwrap();
+    let password = env::var("POSTGRES_PASSWORD").unwrap();
+    let database = env::var("POSTGRES_DATABASE").unwrap();
+
+    debug!("Postgres config host={host}, port={port} user={user} password={password} database={database}");
+
+    return Config::new()
+        .host(host.as_str())
+        .port(port)
+        .user(user.as_str())
+        .password(password.as_str())
+        .dbname(database.as_str()).to_owned();
+}
+
+async fn list_places(State(pool): State<ConnectionPool>) -> impl IntoResponse {
+    let conn = pool.get().await.unwrap();
+    let vec = conn.query("SELECT * FROM places", &[]).await.unwrap();
+    let places = vec.iter()
+        .map(row_to_place)
         .collect::<Vec<_>>();
 
     return Json(places);
 }
 
-async fn create_place(State(db): State<Db>, Json(input): Json<CreatePlace>) -> impl IntoResponse {
+fn row_to_place(r: &Row) -> Place {
+    let id: Uuid = r.get("id");
+    return Place {
+        id,
+        name: "".to_string(),
+    };
+}
+
+async fn create_place(State(pool): State<ConnectionPool>, Json(input): Json<CreatePlace>) -> impl IntoResponse {
     let place = Place {
         id: Uuid::new_v4(),
         name: input.name,
     };
-    db.write().unwrap().insert(place.id, place.clone());
+    let conn = pool.get().await.unwrap();
+    conn.execute("INSERT INTO places (id, name) VALUES ($1, $2)", &[&place.id, &place.name]).await.unwrap();
     return (StatusCode::CREATED, Json(place));
 }
 
